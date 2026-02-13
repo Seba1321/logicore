74a112e4bce33f394de180c64d5a84d5ab09dafc
BenjaminCastro717 <benjamn.castro@uc.cl>
2026-02-12 23:02:08 -0300

    ssss

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/logicore/"
-});
+  base: "/logicore/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
