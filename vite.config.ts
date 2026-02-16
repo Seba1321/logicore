ce4f2171a29cb2755227933f308a1c6c0f5fbea7
ChanchoEnPiedras <robertjosetomas@gmail.com>
2026-02-16 16:38:03 -0300

    chore: remove debug file and rebuild with base /

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/logicore/",
+  base: "/",
   build: { outDir: "docs", emptyOutDir: true },
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
